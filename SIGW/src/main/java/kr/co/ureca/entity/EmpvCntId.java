e463082e8b1f88865d1c632b374784c15c82a778
ChoiJiHooon <prin81208@naver.com>
2024-08-09 17:44:18 +0900

    feat: entity empvcnt 복합키 설정

---
+++ SIGW/src/main/java/kr/co/ureca/entity/EmpvCntId.java
@@ -0,0 +1,42 @@
+package kr.co.ureca.entity;
+
+import jakarta.persistence.Embeddable;
+import java.io.Serializable;
+
+@Embeddable
+public class EmpvCntId implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private VacationType vtype;
+
+    private int empno;
+
+    // 기본 생성자
+    public EmpvCntId() {}
+
+    // 매개변수가 있는 생성자
+    public EmpvCntId(VacationType vtype, int empno) {
+        this.vtype = vtype;
+        this.empno = empno;
+    }
+
+    // equals와 hashCode 메서드는 복합 키 클래스에서 필수입니다.
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        EmpvCntId that = (EmpvCntId) o;
+
+        if (empno != that.empno) return false;
+        return vtype == that.vtype;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = vtype != null ? vtype.hashCode() : 0;
+        result = 31 * result + empno;
+        return result;
+    }
+}
--- SIGW/src/main/java/kr/co/ureca/entity/empvcnt.java
+++ SIGW/src/main/java/kr/co/ureca/entity/empvcnt.java
@@ -1,34 +1,32 @@
 package kr.co.ureca.entity;
 
-
-import java.time.LocalDateTime;
-import java.util.Date;
-
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import jakarta.persistence.*;
 
 @Entity
-@Table(name= "empvcnt")
+@Table(name = "empvcnt")
 @Getter
 @Setter
 @ToString
 public class empvcnt {
-	
-	@Id
-	private VacationType vtype; 
-	
-	@ManyToOne
+
+    @EmbeddedId
+    private EmpvCntId id;
+
+    @MapsId("empno")
+    @ManyToOne
     @JoinColumn(name = "empno", nullable = false)
-    private employee employee;  // 부서와의 관계 설정
-	
-	@Column(name = "vremain",nullable= false)
-	private int vremain;	
-	
-	@Column(name = "vused",nullable= false)
-	private int vused;	
-	
+    private employee employee; // 부서와의 관계 설정
+
+    @Column(name = "vremain", nullable = false)
+    private int vremain;
+
+    @Column(name = "vused", nullable = false)
+    private int vused;
+    
+    // 다른 필요한 메서드들
 }
 
 
